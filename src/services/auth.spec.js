95df222f2f3613d842cd3605b4bd40a1f56e6500
gbederdinov <gbederdinov@matvil.ru>
2017-08-25 15:51:08 +0300

    * start karma testing for auth service

---
+++ src/services/auth.spec.js
@@ -0,0 +1,36 @@
+describe('auth service testing', () => {
+  let service;
+  let mock;
+  let spies = {};
+
+  beforeEach(module('app'));
+  beforeEach(() => {
+    inject($injector => {
+      service = $injector.get('UserService');
+      spies.userResourceGet = chai.spy(service.userResource.get);
+    });
+  });
+
+  it('should return a user', () => {
+    const user = service.user();
+    expect(user).to.have.own.property('name');
+    expect(user).to.have.own.property('email');
+    expect(user).to.have.own.property('money');
+
+    expect(spies.userResourceGet).to.have.been.called();
+  });
+
+  it('should use user resource', () => {
+    service.getUser();
+
+    expect(spies.userResourceGet).to.have.been.called();
+  });
+
+  it('should decrease money', () => {
+    const moneyToSpend = 23;
+    const moneyBefore = service.user().money;
+    service.buy(moneyToSpend);
+    const moneyAfter = moneyBefore - service.user().money;
+    expect(moneyAfter).to.be.equal(moneyToSpend);
+  });
+});
