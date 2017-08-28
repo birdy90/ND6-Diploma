08b7972fdea4b47d97196d9245670c2470639147
gbederdinov <gbederdinov@matvil.ru>
2017-08-28 16:48:57 +0300

    * plus one filter test

--- src/services/filters.spec.js
+++ src/services/filters.spec.js
@@ -12,7 +12,8 @@ describe('filters testing', () => {
     {input: 1, expect: '0:00:01'},
     {input: 100, expect: '0:01:40'},
     {input: 1000, expect: '0:16:40'},
-    {input: 10000, expect: '2:46:40'}
+    {input: 10000, expect: '2:46:40'},
+    {input: 100000, expect: '27:46:40'}
   ];
 
   secondsToStringFilterTests.forEach(test => {
